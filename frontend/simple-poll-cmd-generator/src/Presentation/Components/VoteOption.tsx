3feb53784aba96b36bc234fd8607c9e7aec1eb8e
vkmouse <knight31241@gmail.com>
2022-10-17 17:42:28 +0800

    feat(VoteOption): Add delete button for VoteOption

--- frontend/simple-poll-cmd-generator/src/Presentation/Components/VoteOption.tsx
+++ frontend/simple-poll-cmd-generator/src/Presentation/Components/VoteOption.tsx
@@ -12,7 +12,16 @@ function Option(props: Props) {
   return (
     <Row>
       <Vote type='text' placeholder={placeholder} />
+      <IconButton><DeleteOptionIcon /></IconButton>
     </Row>
+  );
+}
+
+function DeleteOptionIcon() {
+  return (
+    <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
+      <path transform="rotate(45 12 12.001)" id="svg_1" d="m12,2.00098c-5.514,0 -10,4.486 -10,10.00002c0,5.514 4.486,10 10,10c5.514,0 10,-4.486 10,-10c0,-5.51402 -4.486,-10.00002 -10,-10.00002zm5,11.00002l-4,0l0,4l-2,0l0,-4l-4,0l0,-2l4,0l0,-4.00002l2,0l0,4.00002l4,0l0,2z" fill="#FF6060"/>
+    </svg>
   );
 }
 
