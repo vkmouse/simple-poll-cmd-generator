3451459a9381cc594887cddb8f243866669b8be9
vkmouse <knight31241@gmail.com>
2022-10-17 15:54:06 +0800

    refactor(f-app): replace global css Remove css body. And add Global to App component.

--- frontend/simple-poll-cmd-generator/src/App.tsx
+++ frontend/simple-poll-cmd-generator/src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import './App.css';
+import { Global, css } from '@emotion/react'
 import HomeView from './Presentation/Views/HomeView/HomeView';
 
 function App() {
+  const GlobalStyles = css`
+    body {
+      background-color: #2F3136;
+      color: #72767D;
+      margin: 0;
+      font-family: 微軟正黑體;
+    }
+  `
+
   return (
-    <HomeView />
+    <>
+      <Global styles={GlobalStyles} />
+      <HomeView />
+    </>
   );
 }
 
