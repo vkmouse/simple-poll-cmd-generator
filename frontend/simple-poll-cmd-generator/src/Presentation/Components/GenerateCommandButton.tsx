80a6e150b7042934114d2946ba2c49ac304439dc
vkmouse <knight31241@gmail.com>
2022-10-18 11:05:32 +0800

    feat(HomeView): add link to command page This commit add a link to generate command button.

--- frontend/simple-poll-cmd-generator/src/Presentation/Components/GenerateCommandButton.tsx
+++ frontend/simple-poll-cmd-generator/src/Presentation/Components/GenerateCommandButton.tsx
@@ -2,15 +2,18 @@ import styled from '@emotion/styled';
 import React from 'react';
 import Button from '../Styles/Button';
 import Row from '../Styles/Row';
+import { Link } from "react-router-dom";
 
 function GenerateCommandButton() {
   const Container = styled(Row)`
     justify-content: end
   `
-  
+
   return (
     <Container>
-      <Button>產生投票指令</Button>
+      <Link to="/command">
+        <Button>產生投票指令</Button>
+      </Link>
     </Container>
   );
 }
