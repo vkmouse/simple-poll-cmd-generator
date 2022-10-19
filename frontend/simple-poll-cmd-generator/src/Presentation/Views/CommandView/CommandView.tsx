7f97e68638c0827a3db5893822b4a3d46a55d557
vkmouse <knight31241@gmail.com>
2022-10-19 11:04:25 +0800

    feat(CommandView): add layout for CommandView

--- frontend/simple-poll-cmd-generator/src/Presentation/Views/CommandView/CommandView.tsx
+++ frontend/simple-poll-cmd-generator/src/Presentation/Views/CommandView/CommandView.tsx
@@ -1,9 +1,28 @@
-function HomeView() {
+import styled from "@emotion/styled";
+
+const Container = styled.div({
+  backgroundColor: '#40444B',
+  borderRadius: '10px',
+  borderWidth: '0px',
+  marginLeft: '0.5em',
+  marginRight: '0.5em',
+});
+
+const Content = styled.span({
+  color: '#D9DADB',
+  display: 'inline-block',
+  fontSize: '125%',
+  padding: '0.5em',
+});
+
+function CommandView() {
   return (
-    <>
-      123
-    </>
+    <Container> 
+      <Content>
+        Please wait, command is generating...
+      </Content>
+    </Container>
   );
 }
 
-export default HomeView;
+export default CommandView;
